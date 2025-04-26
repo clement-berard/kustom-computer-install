d6cb60d927dbcb071ba2807b15326bacff25660c
Clément BERARD <contact@clementberard.com>
2025-04-26 19:12:41 +0200

    up

--- init.sh
+++ init.sh
@@ -22,7 +22,7 @@ if [ $? -ne 0 ]; then
     return 1
 fi
 
-programs=("zsh" "btop" "bat" "n")
+programs=("zsh" "btop" "bat" "n" "glow")
 
 # Loop to install each program with Homebrew
 for program in "${programs[@]}"; do
