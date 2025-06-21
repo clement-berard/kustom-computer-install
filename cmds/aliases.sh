74eebaffceca63c2aed816f38c25a83ba6afbbe4
Clément BERARD <contact@clementberard.com>
2025-06-21 14:15:09 +0200

    chore: update (#3)

---
+++ cmds/aliases.sh
@@ -0,0 +1,23 @@
+# system
+alias top="btop"
+alias l='eza --long --git --all'
+alias cat='bat'
+
+# pnpm aliases
+alias pp="pnpm"
+alias ppx="pnpx"
+alias ppi="pnpm install"
+alias ppdd="pnpm dedupe"
+alias ppup="pnpm update -Lir"
+
+# yarn
+alias y='yarn'
+alias yd='yarn dev'
+alias ys='yarn start'
+alias yb='yarn build'
+alias ya='yarn add'
+alias yad='yarn add -D'
+alias yup='yarn upgrade-interactive --latest'
+
+# docker
+alias dcu="docker-compose up"
