05eb55a0ed610b4660b1ca25774a4592abb3f782
Clément BERARD <contact@clementberard.com>
2025-11-01 11:11:50 +0100

    chore: update workflow and init (#9)

--- cmds/fs.sh
+++ cmds/fs.sh
@@ -1,8 +1,13 @@
-kc() {
+kc-old() {
     if [ "$1" = "rmd" ]; then
         gum spin --title "Remove recursively 'node_modules' in $(pwd)" -- \
             find . -name "node_modules" -type d -prune -exec rm -rf {} +
     else
         echo "Usage: kc rmd"
     fi
+}
+
+kc() {
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    "$SCRIPT_DIR/go_bin/main_darwin_arm64" "$@"
 }
